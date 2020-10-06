3371fa9ba8f7697a50f2b625e15bebdef6715433
jiroken25 <k.hirota0205@gmail.com>
2020-10-06 23:28:13 +0800

    flask app

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect
+from flask import jsonify
+from flask_pymongo import pymongo
+from config import mongo_pass
+from bson.json_util import dumps
+import json
+
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+conn = f'mongodb+srv://admin:{mongo_pass}@cluster0.7klhs.mongodb.net/job_data?retryWrites=true&w=majority'
+client = pymongo.MongoClient(conn)
+db = client.get_database('job_data')
+col = pymongo.collection.Collection(db, 'details')
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    all_data = json.loads(dumps(col.find()))
+    print(len(all_data))
+
+    # Return template and data
+    return render_template("index.html", reference=all_data)
+
+
+
+@app.route("/<country>")
+def country_data(country):
+
+    country_data = json.loads(dumps(col.find({"country":country})))
+
+    
+    return jsonify(country_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
